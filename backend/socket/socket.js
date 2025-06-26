b0ec736412660acb056da205ba902e803f4a6fd4
Abhijeet Tirkey <abhijeettirkey12@gmail.com>
2025-06-26 21:23:54 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,13 +1,14 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+require("dotenv").config();
 
 const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["http://localhost:3000"],
+		origin: process.env.CLIENT_ORIGIN,
 		methods: ["GET", "POST"],
 	},
 });
